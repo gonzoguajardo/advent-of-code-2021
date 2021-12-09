d5e667fa8d9fa6857d7740273e0aebc7473070c5
Gonzo Guajardo <gonzo.guajardo@gmail.com>
2021-12-09 00:09:52 -0600

    day8 part2 - took me a while..

--- day8-part2/main.go
+++ day8-part2/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -19,156 +20,134 @@ func main() {
 
 	// handle input
 
-	originalCode := []string{
-		"abcefg",
-		"cf",
-		"acdeg",
-		"acdfg",
-		"bcdf",
-		"abdfg",
-		"abdefg",
-		"acf",
-		"abcdefg",
-		"abcdfg",
-	}
+	answerMap := make(map[int]string)
+	letterMap := make(map[string]string)
+	var sum int
 
-	sourceOfTruth := make([][][2]string, 7)
 	for scanner.Scan() {
 		lines := strings.Split(scanner.Text(), "|")
 		var uniqueSignals []string
 		for _, uniqueSignal := range strings.Fields(lines[0]) {
 			switch len(uniqueSignal) {
 			case 2:
-				truth := make([][2]string, 0)
-				truth = append(truth, [2]string{uniqueSignal, originalCode[1]})
-				sourceOfTruth[1] = truth
+				answerMap[1] = uniqueSignal
 			case 3:
-				truth := make([][2]string, 0)
-				truth = append(truth, [2]string{uniqueSignal, originalCode[7]})
-				sourceOfTruth[2] = truth
+				answerMap[7] = uniqueSignal
 			case 4:
-				truth := make([][2]string, 0)
-				truth = append(truth, [2]string{uniqueSignal, originalCode[4]})
-				sourceOfTruth[3] = truth
+				answerMap[4] = uniqueSignal
 			case 7:
-				truth := make([][2]string, 0)
-				truth = append(truth, [2]string{uniqueSignal, originalCode[8]})
-				sourceOfTruth[6] = truth
+				answerMap[8] = uniqueSignal
 			default:
 				uniqueSignals = append(uniqueSignals, uniqueSignal)
 			}
 		}
 
 		workingOutput := make([]string, 0)
-		outputs := make([]int, 4)
-		var numberOfOutputs int
 
-		for index, output := range strings.Fields(lines[1]) {
-			switch len(output) {
-			case 2:
-				numberOfOutputs++
-				outputs[index] = 1
-			case 3:
-				numberOfOutputs++
-				outputs[index] = 7
-			case 4:
-				numberOfOutputs++
-				outputs[index] = 4
-			case 7:
-				numberOfOutputs++
-				outputs[index] = 8
-			default:
-				workingOutput = append(workingOutput, output)
-			}
+		for _, output := range strings.Fields(lines[1]) {
+			workingOutput = append(workingOutput, output)
+
 		}
 
-		//ez solve for a
-		for _, char := range sourceOfTruth[2][0][0] {
-			if !strings.ContainsRune(sourceOfTruth[1][0][0], char) {
-				truth := make([][2]string, 0)
-				truth = append(truth, [2]string{string(char), "a"})
-				sourceOfTruth[0] = truth
+		//fmt.Printf("uniqueSignals: %v\n", uniqueSignals)
+		//fmt.Printf("working output: %v\n", workingOutput)
+		//fmt.Printf("answer: %v\n", answerMap)
+		//fmt.Printf("outputs: %v\n", outputs)
+
+		letterMap["a"] = removeSubstring(answerMap[7], answerMap[1])
+
+		//find 9
+		fourPlusSeven := addUniques(answerMap[4], answerMap[7])
+		uniqueSignalsCopy := make([]string, 0)
+		for _, us := range uniqueSignals {
+			if len(fourPlusSeven)+1 == len(us) && containsSubstring(us, fourPlusSeven) {
+				answerMap[9] = us
+				letterMap["g"] = removeSubstring(us, fourPlusSeven)
+			} else {
+				uniqueSignalsCopy = append(uniqueSignalsCopy, us)
 			}
 		}
+		uniqueSignals = uniqueSignalsCopy
+		//fmt.Printf("uniqueSignals: %v\n", uniqueSignals)
 
-		//ez solve for 2nd pair
-		sourceOfTruth[1] = append(sourceOfTruth[1], [2]string{removeSubstring(sourceOfTruth[3][0][0], sourceOfTruth[2][0][0]),
-			removeSubstring(sourceOfTruth[3][0][1], sourceOfTruth[2][0][1])})
+		letterMap["e"] = removeSubstring(answerMap[8], answerMap[9])
 
-		//loop through and find other solutions
-	signal:
-		for len(uniqueSignals) > 0 {
-			for _, uniqueSignal := range uniqueSignals {
-				output := ""
-				currentLengthCheck := 1
-				for len(uniqueSignal) > 0 {
-					if currentLengthCheck > 6 {
-						break
-					}
-					//fmt.Printf("unique signal: %v\n", uniqueSignal)
-					//fmt.Printf("output: %v\n", output)
-					//fmt.Printf("currentLengthCheck: %v\n", currentLengthCheck)
-					for _, truth := range sourceOfTruth[currentLengthCheck-1] {
-						if containsSubstring(uniqueSignal, truth[0]) {
-							uniqueSignal = removeSubstring(uniqueSignal, truth[0])
-							output += truth[1]
-							fmt.Println(uniqueSignal)
-							fmt.Println(output)
-						}
-					}
-					currentLengthCheck++
-
-					if len(uniqueSignal) == 1 {
-					originalCode:
-						for _, oc := range originalCode {
-							var count int
-							if containsSubstring(oc, output) && len(output)+1 == len(oc) {
-								count++
-							}
-							if count == 1 {
-								for _, check := range sourceOfTruth[0] {
-									if check[0] == uniqueSignal {
-										continue originalCode
-									}
-								}
-
-								elems := [2]string{uniqueSignal, removeSubstring(oc, output)}
-								fmt.Println(elems)
-								sourceOfTruth[0] = append(sourceOfTruth[0], elems)
-								fmt.Printf("source of truth: %v\n", sourceOfTruth)
-
-								if len(sourceOfTruth[0]) == 7 {
-									break signal
-								}
-							}
-						}
-
-					}
-				}
-
+		//find zero
+		almostZero := answerMap[1] + letterMap["a"] + letterMap["e"] + letterMap["g"]
+		uniqueSignalsCopy = make([]string, 0)
+		for _, us := range uniqueSignals {
+			if len(almostZero)+1 == len(us) && containsSubstring(us, almostZero) {
+				answerMap[0] = us
+				letterMap["b"] = removeSubstring(us, almostZero)
+			} else {
+				uniqueSignalsCopy = append(uniqueSignalsCopy, us)
 			}
 		}
+		uniqueSignals = uniqueSignalsCopy
+		//fmt.Printf("uniqueSignals: %v\n", uniqueSignals)
 
-		for indexOutput, output := range workingOutput {
-			fmt.Println(output)
-			converted := convert(output, sourceOfTruth[0])
-			fmt.Println(converted)
-			for index, check := range originalCode {
-				if check == converted {
-					outputs[indexOutput] = index
+		letterMap["d"] = removeSubstring(answerMap[8], answerMap[0])
+
+		//find three
+		three := answerMap[1] + letterMap["a"] + letterMap["d"] + letterMap["g"]
+		uniqueSignalsCopy = make([]string, 0)
+		for _, us := range uniqueSignals {
+			if len(three) == len(us) && containsSubstring(three, us) {
+				answerMap[3] = us
+			} else {
+				uniqueSignalsCopy = append(uniqueSignalsCopy, us)
+			}
+		}
+		uniqueSignals = uniqueSignalsCopy
+
+		//find six
+		uniqueSignalsCopy = make([]string, 0)
+		for _, us := range uniqueSignals {
+			if len(us) == 6 {
+				answerMap[6] = us
+			} else {
+				uniqueSignalsCopy = append(uniqueSignalsCopy, us)
+			}
+		}
+		uniqueSignals = uniqueSignalsCopy
+
+		//find c
+		letterMap["c"] = removeSubstring(answerMap[8], answerMap[6])
+
+		//2 and 5
+		uniqueSignalsCopy = make([]string, 0)
+		for _, us := range uniqueSignals {
+			if containsSubstring(us, letterMap["c"]) {
+				answerMap[2] = us
+			} else {
+				answerMap[5] = us
+			}
+		}
+		uniqueSignals = uniqueSignalsCopy
+
+		//fmt.Printf("answer: %v\n", answerMap)
+		//fmt.Printf("letterMap: %v\n", letterMap)
+		//fmt.Printf("uniqueSignals: %v\n", uniqueSignals)
+
+		outputValueString := ""
+		for _, output := range workingOutput {
+			for k, v := range answerMap {
+				if len(v) == len(output) && containsSubstring(output, v) {
+					outputValueString += strconv.Itoa(k)
 				}
 			}
 		}
 
-		fmt.Printf("source of truth: %v\n", sourceOfTruth)
-		fmt.Printf("uniqueSignals: %v\n", uniqueSignals)
-		fmt.Printf("working output: %v\n", workingOutput)
-		fmt.Printf("outputs: %v\n", outputs)
+		outputValue, _ := strconv.Atoi(outputValueString)
+		sum += outputValue
+		fmt.Println(outputValueString)
 
+		//break
 	}
 
-}
+	fmt.Println(sum)
 
+}
 func containsSubstring(str string, substr string) bool {
 	for _, char := range substr {
 		if !strings.ContainsRune(str, char) {
@@ -177,22 +156,22 @@ func containsSubstring(str string, substr string) bool {
 	}
 	return true
 }
-
 func removeSubstring(str string, substr string) string {
 	for _, char := range substr {
 		str = strings.ReplaceAll(str, string(char), "")
 	}
 	return str
 }
-
-func convert(str string, keys [][2]string) string {
+func addUniques(string1 string, string2 string) string {
 	var output string
-	for _, char := range str {
-		charString := string(char)
-		for _, key := range keys {
-			if charString == key[0] {
-				output += key[1]
-			}
+	for _, char := range string1 {
+		if !strings.ContainsRune(output, char) {
+			output += string(char)
+		}
+	}
+	for _, char := range string2 {
+		if !strings.ContainsRune(output, char) {
+			output += string(char)
 		}
 	}
 	return output
